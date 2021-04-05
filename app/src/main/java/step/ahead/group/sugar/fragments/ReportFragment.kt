57bfa52c92e8ea423f51e4ed38d5663a6e6ca774
ziad00100 <ziad00100ziad@gmail.com>
2021-04-05 05:01:49 +0300

    update fragments: MoreFragment.kt,NotificationFragment,PersonalSettingFragment.kt,ReportFragment.kt
    Editing all new names fragments to work perfectly .

--- app/src/main/java/step/ahead/group/sugar/fragments/ReportFragment.kt
+++ app/src/main/java/step/ahead/group/sugar/fragments/ReportFragment.kt
@@ -24,7 +24,7 @@ class ReportFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_reports_fragment, container, false)
+        return inflater.inflate(R.layout.reports_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
